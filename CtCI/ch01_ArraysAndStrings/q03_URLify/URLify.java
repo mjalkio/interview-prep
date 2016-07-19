e0863cfe355458cdbd02558f62c8aeafad2de0d3
Michael Jalkio <mjalkio@gmail.com>
2016-07-19 12:00:37 -0500

    Write tests for ch01q03 URLify

---
+++ CtCI/ch01_ArraysAndStrings/q03_URLify/URLify.java
@@ -0,0 +1,10 @@
+package ch01_ArraysAndStrings.q03_URLify;
+
+public class URLify {
+
+	public static Object urlify(char[] charArray, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CtCI/ch01_ArraysAndStrings/q03_URLify/URLifyTest.java
@@ -0,0 +1,24 @@
+package ch01_ArraysAndStrings.q03_URLify;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class URLifyTest {
+	@Test
+	public void test() {
+		assertEquals("Mr%20John%20Smith".toCharArray(),
+				URLify.urlify("Mr John Smith    ".toCharArray(), 13));
+		assertEquals("%20".toCharArray(), URLify.urlify("   ".toCharArray(), 1));
+		assertEquals("hello%20%20%20world".toCharArray(),
+				URLify.urlify("hello   world      ".toCharArray(), 13));
+		assertEquals("test".toCharArray(),
+				URLify.urlify("test".toCharArray(), 4));
+		assertEquals("%20%20".toCharArray(),
+				URLify.urlify("      ".toCharArray(), 2));
+		assertEquals("why%20".toCharArray(),
+				URLify.urlify("why   ".toCharArray(), 1));
+		assertEquals("%20lol".toCharArray(),
+				URLify.urlify(" lol  ".toCharArray(), 4));
+	}
+}
