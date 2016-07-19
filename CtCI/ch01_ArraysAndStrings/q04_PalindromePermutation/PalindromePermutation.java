04470d8f467dd80f28b258042b19f4134e7cbdbf
Michael Jalkio <mjalkio@gmail.com>
2016-07-19 13:05:00 -0500

    Solve ch01q04 PalindromePermutation

--- CtCI/ch01_ArraysAndStrings/q04_PalindromePermutation/PalindromePermutation.java
+++ CtCI/ch01_ArraysAndStrings/q04_PalindromePermutation/PalindromePermutation.java
@@ -1,9 +1,39 @@
 package ch01_ArraysAndStrings.q04_PalindromePermutation;
 
+import java.util.HashMap;
+
 public class PalindromePermutation {
 
 	public static boolean palindromePermutation(String s) {
-		// TODO Auto-generated method stub
+		if (s == null || s.length() == 0) {
+			return false;
+		}
+
+		s = s.toLowerCase();
+		HashMap<Character, Integer> charCounts = new HashMap<Character, Integer>();
+
+		for (char c : s.toCharArray()) {
+			if (c != ' ') {
+				if (charCounts.containsKey(c)) {
+					int newCount = charCounts.get(c) + 1;
+					charCounts.put(c, newCount);
+				} else {
+					charCounts.put(c, 1);
+				}
+			}
+		}
+
+		boolean foundPivot = false;
+		for (Character c : charCounts.keySet()) {
+			if (charCounts.get(c) % 2 == 1) {
+				if (foundPivot) {
+					return false;
+				} else {
+					foundPivot = true;
+				}
+			}
+		}
+
 		return true;
 	}
 
