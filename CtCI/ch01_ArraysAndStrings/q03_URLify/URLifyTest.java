6c50ad0793a5f3b9cf0bb281b10be4bb93980d47
Michael Jalkio <mjalkio@gmail.com>
2016-07-19 12:23:57 -0500

    Fix URLify tests by using assertArrayEquals

--- CtCI/ch01_ArraysAndStrings/q03_URLify/URLifyTest.java
+++ CtCI/ch01_ArraysAndStrings/q03_URLify/URLifyTest.java
@@ -1,30 +1,31 @@
 package ch01_ArraysAndStrings.q03_URLify;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
 public class URLifyTest {
 	@Test
 	public void test() {
-		assertEquals("Mr%20John%20Smith".toCharArray(),
+		assertArrayEquals("Mr%20John%20Smith".toCharArray(),
 				URLify.urlify("Mr John Smith    ".toCharArray(), 13));
-		assertEquals("%20".toCharArray(), URLify.urlify("   ".toCharArray(), 1));
-		assertEquals("hello%20%20%20world".toCharArray(),
+		assertArrayEquals("%20".toCharArray(),
+				URLify.urlify("   ".toCharArray(), 1));
+		assertArrayEquals("hello%20%20%20world".toCharArray(),
 				URLify.urlify("hello   world      ".toCharArray(), 13));
-		assertEquals("test".toCharArray(),
+		assertArrayEquals("test".toCharArray(),
 				URLify.urlify("test".toCharArray(), 4));
-		assertEquals("%20%20".toCharArray(),
+		assertArrayEquals("%20%20".toCharArray(),
 				URLify.urlify("      ".toCharArray(), 2));
-		assertEquals("why%20".toCharArray(),
-				URLify.urlify("why   ".toCharArray(), 1));
-		assertEquals("%20lol".toCharArray(),
+		assertArrayEquals("why%20".toCharArray(),
+				URLify.urlify("why   ".toCharArray(), 4));
+		assertArrayEquals("%20lol".toCharArray(),
 				URLify.urlify(" lol  ".toCharArray(), 4));
-		assertEquals("%20lol".toCharArray(),
+		assertArrayEquals("%20lol".toCharArray(),
 				URLify.urlify(" lol  ".toCharArray(), 4));
-		assertEquals("test ".toCharArray(),
+		assertArrayEquals("test ".toCharArray(),
 				URLify.urlify("test ".toCharArray(), 4));
-		assertEquals("t%20est ".toCharArray(),
+		assertArrayEquals("t%20est ".toCharArray(),
 				URLify.urlify("t est   ".toCharArray(), 5));
 	}
 }
