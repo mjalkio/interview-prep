d330ccae0785e4f8f0becbfd8c3c1e4fc2106a0c
Michael Jalkio <mjalkio@gmail.com>
2016-07-19 12:24:18 -0500

    Solve ch01q03 URLify

--- CtCI/ch01_ArraysAndStrings/q03_URLify/URLify.java
+++ CtCI/ch01_ArraysAndStrings/q03_URLify/URLify.java
@@ -2,9 +2,28 @@ package ch01_ArraysAndStrings.q03_URLify;
 
 public class URLify {
 
-	public static Object urlify(char[] charArray, int i) {
-		// TODO Auto-generated method stub
-		return null;
+	public static char[] urlify(char[] input, int length) {
+		int numSpaces = 0;
+		for (int i = 0; i < length; i++) {
+			if (input[i] == ' ') {
+				numSpaces++;
+			}
+		}
+
+		int location = length - 1 + numSpaces * 2;
+		for (int i = length - 1; i >= 0; i--) {
+			if (input[i] == ' ') {
+				location = location - 2;
+				input[location] = '%';
+				input[location + 1] = '2';
+				input[location + 2] = '0';
+			} else {
+				input[location] = input[i];
+			}
+			location--;
+		}
+
+		return input;
 	}
 
 }
