8d19842c06e0db957bdc80de51ca547b3292dd02
Michael Jalkio <mjalkio@gmail.com>
2016-07-22 23:51:25 -0500

    Solve ch01q06 StringCompression

--- CtCI/ch01_ArraysAndStrings/q06_StringCompression/StringCompression.java
+++ CtCI/ch01_ArraysAndStrings/q06_StringCompression/StringCompression.java
@@ -2,9 +2,24 @@ package ch01_ArraysAndStrings.q06_StringCompression;
 
 public class StringCompression {
 
-	public static Object stringCompression(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public static String stringCompression(String s) {
+		String comp = "";
+		int i = 0;
+		while (i < s.length()) {
+			char c = s.charAt(i);
+			int charCount = 1;
+			while (i < s.length() - 1 && s.charAt(i + 1) == c) {
+				charCount++;
+				i++;
+			}
+			comp = comp + c + charCount;
+			i++;
+		}
+		if (comp.length() < s.length()) {
+			return comp;
+		} else {
+			return s;
+		}
 	}
 
 }
