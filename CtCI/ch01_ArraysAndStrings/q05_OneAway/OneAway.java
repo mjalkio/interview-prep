c5f231c9b33dad4a0a69ca53993f75fad48ac3bc
Michael Jalkio <mjalkio@gmail.com>
2016-07-22 23:23:42 -0500

    Solve ch01q05 OneAway

--- CtCI/ch01_ArraysAndStrings/q05_OneAway/OneAway.java
+++ CtCI/ch01_ArraysAndStrings/q05_OneAway/OneAway.java
@@ -2,9 +2,46 @@ package ch01_ArraysAndStrings.q05_OneAway;
 
 public class OneAway {
 
-	public static boolean oneAway(String string, String string2) {
-		// TODO Auto-generated method stub
-		return false;
+	public static boolean oneAway(String s1, String s2) {
+		if (s1.equals(s2)) {
+			return true;
+		} else if (s1.length() == s2.length()) {
+			boolean foundDifference = false;
+			for (int i = 0; i < s1.length(); i++) {
+				if (s1.charAt(i) != s2.charAt(i)) {
+					if (foundDifference) {
+						return false;
+					} else {
+						foundDifference = true;
+					}
+				}
+			}
+			return true;
+		} else if (Math.abs(s1.length() - s2.length()) > 1) {
+			return false;
+		} else {
+			String longer, shorter;
+			if (s1.length() > s2.length()) {
+				longer = s1;
+				shorter = s2;
+			} else {
+				longer = s2;
+				shorter = s1;
+			}
+			boolean foundAdded = false;
+			for (int i = 0; i < shorter.length(); i++) {
+				if (foundAdded) {
+					if (shorter.charAt(i) != longer.charAt(i + 1)) {
+						return false;
+					}
+				} else {
+					if (shorter.charAt(i) != longer.charAt(i)) {
+						foundAdded = true;
+					}
+				}
+			}
+			return true;
+		}
 	}
 
 }
