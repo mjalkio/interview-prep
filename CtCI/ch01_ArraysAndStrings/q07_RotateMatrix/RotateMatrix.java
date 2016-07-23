70c323ac0b3947491aca6116a9c79bf1ffa7e1ec
Michael Jalkio <mjalkio@gmail.com>
2016-07-23 01:08:53 -0500

    Solve ch01q07 RotateMatrix

--- CtCI/ch01_ArraysAndStrings/q07_RotateMatrix/RotateMatrix.java
+++ CtCI/ch01_ArraysAndStrings/q07_RotateMatrix/RotateMatrix.java
@@ -1,10 +1,28 @@
 package ch01_ArraysAndStrings.q07_RotateMatrix;
 
 public class RotateMatrix {
-
-	public static int[][] rotateMatrix(int[][] is) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * O(n^2) solution that uses O(1) space.
+	 * 
+	 * Note: should really be a void method. Using int[][] for ease of testing
+	 * #badStyleSorry
+	 * 
+	 * @param matrix
+	 *            an n by n matrix
+	 * @return the same matrix rotated 90 degrees
+	 */
+	public static int[][] rotateMatrix(int[][] matrix) {
+		for (int layer = 0; layer < matrix.length / 2; layer++) {
+			int last = matrix.length - 1 - layer;
+			for (int i = layer; i < last; i++) {
+				int offset = i - layer;
+				int temp = matrix[layer][i];
+				matrix[layer][i] = matrix[last - offset][layer];
+				matrix[last - offset][layer] = matrix[last][last - offset];
+				matrix[last][last - offset] = matrix[i][last];
+				matrix[i][last] = temp;
+			}
+		}
+		return matrix;
 	}
-
 }
