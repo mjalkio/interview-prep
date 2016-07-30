c4f8fcdfc7c71e0b2755d38def864c6ed004eb5f
Michael Jalkio <mjalkio@gmail.com>
2016-07-29 21:26:18 -0500

    Create tests for ch02q01 RemoveDups

---
+++ CtCI/ch02_LinkedLists/q01_RemoveDups/RemoveDupsTest.java
@@ -0,0 +1,42 @@
+package ch02_LinkedLists.q01_RemoveDups;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ch02_LinkedLists.Node;
+
+public class RemoveDupsTest {
+	@Test
+	public void testRemoveDups() {
+		Node lst = new Node(1);
+		assertEquals("1", lst.toString());
+		RemoveDups.removeDups(lst);
+		assertEquals("1", lst.toString());
+
+		lst.appendToTail(1);
+		assertEquals("1,1", lst.toString());
+		RemoveDups.removeDups(lst);
+		assertEquals("1", lst.toString());
+
+		lst.appendToTail(1);
+		lst.appendToTail(2);
+		lst.appendToTail(2);
+		assertEquals("1,1,2,2", lst.toString());
+		RemoveDups.removeDups(lst);
+		assertEquals("1,2", lst.toString());
+
+		lst.appendToTail(1);
+		lst.appendToTail(2);
+		assertEquals("1,2,1,2", lst.toString());
+		RemoveDups.removeDups(lst);
+		assertEquals("1,2", lst.toString());
+
+		lst.appendToTail(3);
+		lst.appendToTail(3);
+		lst.appendToTail(3);
+		assertEquals("1,2,3,3,3", lst.toString());
+		RemoveDups.removeDups(lst);
+		assertEquals("1,2,3", lst.toString());
+	}
+}
