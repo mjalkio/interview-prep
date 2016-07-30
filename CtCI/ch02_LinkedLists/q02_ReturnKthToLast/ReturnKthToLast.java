ef2375abbf0e90b6e3468937a5a968b06298ce99
Michael Jalkio <mjalkio@gmail.com>
2016-07-29 22:17:30 -0500

    Solve ch02q02 ReturnKthToLast

--- CtCI/ch02_LinkedLists/q02_ReturnKthToLast/ReturnKthToLast.java
+++ CtCI/ch02_LinkedLists/q02_ReturnKthToLast/ReturnKthToLast.java
@@ -4,9 +4,24 @@ import ch02_LinkedLists.Node;
 
 public class ReturnKthToLast {
 
-	public static Node kthToLast(int i, Node lst) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Node kthToLast(int k, Node lst) {
+		int size = 0;
+		Node n = lst;
+		while (n != null) {
+			size++;
+			n = n.next;
+		}
+		if (k >= size) {
+			return null;
+		}
+		int index = size - k - 1;
+		n = lst;
+		int i = 0;
+		while (i < index) {
+			n = n.next;
+			i++;
+		}
+		return n;
 	}
 
 }
