18222187ba63e9d8543090e11ff3096d272ea5b0
Michael Jalkio <mjalkio@gmail.com>
2016-07-30 23:26:14 -0500

    Solve ch02q04 Partition

--- CtCI/ch02_LinkedLists/q04_Partition/Partition.java
+++ CtCI/ch02_LinkedLists/q04_Partition/Partition.java
@@ -4,9 +4,23 @@ import ch02_LinkedLists.Node;
 
 public class Partition {
 
-	public static Node partition(Node lst, int i) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Node partition(Node lst, int x) {
+		Node head = lst;
+		Node tail = lst;
+
+		while (lst != null) {
+			Node next = lst.next;
+			if (lst.data < x) {
+				lst.next = head;
+				head = lst;
+			} else {
+				tail.next = lst;
+				tail = lst;
+			}
+			lst = next;
+		}
+		tail.next = null;
+		return head;
 	}
 
 }
