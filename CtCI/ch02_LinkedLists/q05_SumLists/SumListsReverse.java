5c820147eef7fd2f6bb9cdf48e69a9469f22a35f
Michael Jalkio <mjalkio@gmail.com>
2016-08-04 10:17:22 -0500

    Solve ch02q05 SumLists

--- CtCI/ch02_LinkedLists/q05_SumLists/SumListsReverse.java
+++ CtCI/ch02_LinkedLists/q05_SumLists/SumListsReverse.java
@@ -5,8 +5,43 @@ import ch02_LinkedLists.Node;
 public class SumListsReverse {
 
 	public static Node sumLists(Node listOne, Node listTwo) {
-		// TODO Auto-generated method stub
-		return null;
+		Node sum = null;
+		Node sumTail = null;
+		boolean carry = false;
+		while (listOne != null || listTwo != null) {
+			int digit;
+			if (carry) {
+				digit = 1;
+			} else {
+				digit = 0;
+			}
+
+			if (listOne != null) {
+				digit += listOne.data;
+				listOne = listOne.next;
+			}
+			if (listTwo != null) {
+				digit += listTwo.data;
+				listTwo = listTwo.next;
+			}
+
+			if (digit >= 10) {
+				digit -= 10;
+				carry = true;
+			} else {
+				carry = false;
+			}
+
+			if (sum == null) {
+				sum = new Node(digit);
+				sumTail = sum;
+			} else {
+				sumTail.next = new Node(digit);
+				sumTail = sumTail.next;
+			}
+		}
+
+		return sum;
 	}
 
 }
