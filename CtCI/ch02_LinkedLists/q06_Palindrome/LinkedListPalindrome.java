ad05a479342f30d9e65ce5c25f664d0b1153fb49
Michael Jalkio <mjalkio@gmail.com>
2016-08-04 10:57:47 -0500

    Solve ch02q06 Palindrome

--- CtCI/ch02_LinkedLists/q06_Palindrome/LinkedListPalindrome.java
+++ CtCI/ch02_LinkedLists/q06_Palindrome/LinkedListPalindrome.java
@@ -1,12 +1,26 @@
 package ch02_LinkedLists.q06_Palindrome;
 
+import java.util.ArrayList;
+
 import ch02_LinkedLists.Node;
 
 public class LinkedListPalindrome {
 
 	public static boolean isPalindrome(Node list) {
-		// TODO Auto-generated method stub
-		return false;
+		if (list == null) {
+			return false;
+		}
+		ArrayList<Integer> listCopy = new ArrayList<Integer>();
+		while (list != null) {
+			listCopy.add(list.data);
+			list = list.next;
+		}
+		for (int i = 0; i < listCopy.size() / 2; i++) {
+			if (!(listCopy.get(i).equals(listCopy.get(listCopy.size() - 1 - i)))) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
