c1831c343caf358bef631c91c517e37bda2adcbc
Michael Jalkio <mjalkio@gmail.com>
2016-08-10 11:24:04 -0500

    Solve ch03q03 SetOfStacks

--- CtCI/ch03_StacksAndQueues/q03_SetOfStacks/SetOfStacks.java
+++ CtCI/ch03_StacksAndQueues/q03_SetOfStacks/SetOfStacks.java
@@ -1,6 +1,7 @@
 package ch03_StacksAndQueues.q03_SetOfStacks;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 import ch03_StacksAndQueues.MyStack;
 
@@ -16,18 +17,41 @@ public class SetOfStacks<T> {
 	}
 
 	public T pop() {
-		return null;
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+
+		currentStackSize--;
+		MyStack<T> currentStack = stacks.get(stacks.size() - 1);
+		if (currentStackSize == 0) {
+			stacks.remove(stacks.size() - 1);
+			currentStackSize = maxSize;
+		}
+		return currentStack.pop();
 	}
 
 	public void push(T item) {
+		if (isEmpty() || currentStackSize == maxSize) {
+			stacks.add(new MyStack<T>());
+			currentStackSize = 0;
+		}
+
+		MyStack<T> currentStack = stacks.get(stacks.size() - 1);
+		currentStack.push(item);
+		currentStackSize++;
 	}
 
 	public T peek() {
-		return null;
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+
+		MyStack<T> currentStack = stacks.get(stacks.size() - 1);
+		return currentStack.peek();
 	}
 
 	public boolean isEmpty() {
-		return true;
+		return stacks.isEmpty();
 	}
 
 }
