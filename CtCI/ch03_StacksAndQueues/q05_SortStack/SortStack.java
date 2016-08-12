1ab3534146132de5916ac1bab29fed194eb36386
Michael Jalkio <mjalkio@gmail.com>
2016-08-12 13:54:47 -0500

    Solve ch03q05 SortStack

--- CtCI/ch03_StacksAndQueues/q05_SortStack/SortStack.java
+++ CtCI/ch03_StacksAndQueues/q05_SortStack/SortStack.java
@@ -5,8 +5,18 @@ import ch03_StacksAndQueues.MyStack;
 public class SortStack {
 
 	public static void sortStack(MyStack<Integer> stack) {
-		// TODO Auto-generated method stub
+		MyStack<Integer> sorted = new MyStack<Integer>();
 
+		while (!stack.isEmpty()) {
+			int nextNum = stack.pop();
+			while (!sorted.isEmpty() && nextNum < sorted.peek()) {
+				stack.push(sorted.pop());
+			}
+			sorted.push(nextNum);
+		}
+		while (!sorted.isEmpty()) {
+			stack.push(sorted.pop());
+		}
 	}
 
 }
